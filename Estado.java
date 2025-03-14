74834b59190ebee211fec0d4de3472ab5749708d
daniela <daniela.cervantes|@>
2025-03-14 11:15:00 +0100

    generador_estado_inicial

--- Estado.java
+++ Estado.java
@@ -1,7 +1,8 @@
 import IA.Red.*;
 import java.util.*;
+import java.util.Random;
 
-public class EstadoInicial {
+public class Estado {
 
     /* array lists */
     private Sensores sensor;
@@ -22,7 +23,7 @@ public class EstadoInicial {
     private int[] contador_conexiones;
 
     /*Constructor*/
-    public EstadoInicial (int nsensores, int semilla, int ncentros) {
+    public Estado (int nsensores, int semilla, int ncentros) {
         this.sensor = new Sensores(nsensores, semilla);
         this.centros = new CentrosDatos(ncentros, semilla);
         this.conexiones = new int[nsensores] ;
@@ -69,6 +70,25 @@ public class EstadoInicial {
         return conexiones[sensorId] != -1;
     }
 
+    private double dist(int id1, int id2, boolean escentro) {
+        double x1, y1, x2, y2;
+
+        /* id1 siempre será sensor */
+
+        x1 = sensor.get(id1).getCoordX();
+        y1 = sensor.get(id1).getCoordY();
+
+        if (escentro){
+            x2 = centros.get(id2).getCoordX();
+            y2 = centros.get(id2).getCoordY();
+        }
+        else {
+            x2 = sensor.get(id2).getCoordX();
+            y2 = sensor.get(id2).getCoordY();
+        }
+        return Math.sqrt((Math.pow(2,(x1-x2))) + (Math.pow(2,(x2-y2))));
+    }
+
     /*Métodos públicos*/
     /**
      * Conectamos id1 (origen) con id2 (destino)
@@ -82,10 +102,10 @@ public class EstadoInicial {
             return;
         }
 
-        if (esta_transmitiendo(id1)) {
+        /*if (esta_transmitiendo(id1)) {
             System.out.println("Error: El sensor " + id1 + "ya está transmitiendo información.");
             return;
-        }
+        }*/
 
         if (conexiones[id1] == id2) {
             System.out.println("Error: La conexión entre " + id1 + " y " + id2 + " ya está establecida.");
@@ -97,8 +117,14 @@ public class EstadoInicial {
             return;
         }
 
+        if (esta_transmitiendo(id1)) {
+            romper_conexion(id1);
+        }
+
         ++contador_conexiones[id2];
         conexiones[id1] = id2;
+
+        System.out.println("Se ha creado la conexión entre " + id1 + " y " + id2);
     }
 
     /*
@@ -118,12 +144,67 @@ public class EstadoInicial {
         int id2 = conexiones[id1];
         --contador_conexiones[id2];
         conexiones[id1] = -1;
+
+        System.out.println("Se ha roto la conexión entre " + id1 + " y " + id2);
     }
 
     public boolean conexion_con_centro(int sensorId) {
         boolean[] visitado = new boolean[sensor.size()];
         Arrays.fill(visitado, false);
         return dfs(sensorId, visitado);
+    }
+
+    /* esta función genera el estado inicial aleatoriamente */
+    public void estado_inicial_random () {
+        Random rand = new Random();
+
+        for (int i = 0; i < sensor.size(); i++) {
+            int centroId = sensor.size() + rand.nextInt(centros.size());
+            if (es_valido_centros(centroId)) {
+                conexiones[i] = centroId;
+                ++contador_conexiones[centroId];
+            } else {
+                int sensorId = rand.nextInt(sensor.size());
+                while (!es_valido_sensor(sensorId)) {
+                    sensorId = rand.nextInt(sensor.size());
+                }
+                conexiones[i] = sensorId;
+                ++contador_conexiones[sensorId];
+            }
+
+        }
+    }
+
+    /* esta función genera el estado inicial conectando al centro más cercano y teniendo en cuenta las restricciones */
+    public void estado_inicial_cercania () {
+        for (int i = 0; i < sensor.size(); ++i) {
+            double min = Double.POSITIVE_INFINITY;
+            int id_min = -1;
+
+            for (int j = 0; j < centros.size(); ++j) {
+                if (es_valido_centros(j + sensor.size())) {
+                    double distancia = dist(i, j, true);
+                    if (distancia < min) {
+                        min = distancia;
+                        id_min = j + sensor.size();
+                    }
+                }
+            }
+            if (id_min != -1) {
+                conexiones[i] = id_min;
+                ++contador_conexiones[id_min];
+            }
+
+            else { // si no se puede conectar a un centro entonces se conecta a un sensor random
+                Random rand = new Random();
+                int sensorId = rand.nextInt(sensor.size());
+                while (!es_valido_sensor(sensorId)) {
+                    sensorId = rand.nextInt(sensor.size());
+                }
+                conexiones[i] = sensorId;
+                ++contador_conexiones[sensorId];
+            }
+        }
     }
 
     /* mover estos métodos a la clase del algoritmo
