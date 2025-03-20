708e395a6ae08bf8135988512222ee99667701dd
daniela <daniela.cervantes|@>
2025-03-20 12:12:10 +0100

    clase estado acabada a falta de revisar que funcione

--- Estado.java
+++ Estado.java
@@ -1,8 +1,10 @@
 import IA.Red.*;
 import java.util.*;
-import java.util.Random;
 
 public class Estado implements Cloneable {
+    //arrays constantes para bfs
+    private static final int[] X = {-1, 0, 1, 0};
+    private static final int[] Y = {0, 1, 0, -1};
 
     /* array lists */
     private Sensores sensor;
@@ -22,15 +24,44 @@ public class Estado implements Cloneable {
      */
     private int[] contador_conexiones;
 
+    /**
+     * Tablero 100 x 100
+     */
+    int[][] tablero;
+
+    int info_perdida;
+
     /*Constructor*/
     public Estado (int nsensores, int semilla, int ncentros) {
         this.sensor = new Sensores(nsensores, semilla);
         this.centros = new CentrosDatos(ncentros, semilla);
         this.conexiones = new int[nsensores] ;
-        this.capacidadRestante = new double[nsensores];
         Arrays.fill(conexiones, -1);
+        this.capacidadRestante = new double[nsensores + ncentros];
+        for(int i = 0; i < sensor.size(); i++){
+            capacidadRestante[i] = sensor.get(i).getCapacidad()*2;
+        }
+        for(int i = sensor.size(); i < sensor.size() + centros.size(); ++i) {
+            capacidadRestante[i] = 125;
+        }
         this.contador_conexiones = new int[nsensores+ncentros];
         Arrays.fill(contador_conexiones, 0);
+        this.tablero = new int[100][100];
+        for (int i = 0; i < 100; i++) {
+            Arrays.fill(tablero[i], -1); //inicializamos matriz a -1
+        }
+        int posX, posY;
+        for (int i = 0; i < nsensores; ++i) {
+            posX = sensor.get(i).getCoordX();
+            posY = sensor.get(i).getCoordY();
+            tablero[posX][posY] = i;
+        }
+        for (int j = 0; j < ncentros; ++j) {
+            posX = centros.get(j).getCoordX();
+            posY = centros.get(j).getCoordY();
+            tablero[posX][posY] = j+nsensores;
+        }
+        this.info_perdida = 0;
     }
 
     /*Métodos privados*/
@@ -40,9 +71,45 @@ public class Estado implements Cloneable {
         if (!visitado[sensorId]) {
             visitado[sensorId] = true;
             int v = conexiones[sensorId];
-            dfs(v, visitado);
+            //dfs(v, visitado);ç
+            if (v != -1 && dfs(v, visitado)) {
+                return true;
+            }
         }
         return false;
+    }
+
+    private boolean pos_valida(int[][]tablero, int x, int y) {
+        int filas = tablero.length;
+        int columnas = tablero[0].length;
+        return x >= 0 && x < filas && y >= 0 && y < columnas;
+    }
+
+    private ArrayList<Integer> bfs(int sensorId, boolean[][] visitado) {
+        Queue<Pair<Integer, Integer>> cola = new LinkedList<>();
+        cola.add(new Pair<>(sensor.get(sensorId).getCoordX(), sensor.get(sensorId).getCoordY()));
+        visitado[sensor.get(sensorId).getCoordX()][sensor.get(sensorId).getCoordY()] = true;
+        ArrayList<Integer> cercanos = new ArrayList<>();
+
+        while (!cola.isEmpty()) {
+            Pair<Integer, Integer> actual = cola.poll();
+            int posX = actual.first;
+            int posY = actual.second;
+            for (int i = 0; i < 4; ++i) {
+                int x_next = posX + X[i];
+                int y_next = posY + Y[i];
+                if (pos_valida(tablero, x_next, y_next)) {
+                    if (tablero[x_next][y_next] != -1 ) {
+                        cercanos.add(tablero[x_next][y_next]);
+                    }
+                    if (!visitado[x_next][y_next]) {
+                        cola.add(new Pair<>(x_next, y_next));
+                        visitado[x_next][y_next] = true;
+                    }
+                }
+            }
+        }
+        return cercanos;
     }
 
     private boolean es_sensor(int sensorId) {
@@ -71,6 +138,27 @@ public class Estado implements Cloneable {
         return conexiones[sensorId] != -1;
     }
 
+    private void actualizar_capacidadRestante_centro(int sensorId, int centroId) {
+        if (sensor.get(sensorId).getCapacidad() > capacidadRestante[centroId]) {
+            info_perdida += (int) sensor.get(sensorId).getCapacidad() - (int) capacidadRestante[centroId];
+            capacidadRestante[centroId] = 0;
+        }
+        else capacidadRestante[centroId] -= sensor.get(sensorId).getCapacidad();
+    }
+
+    //el 1 le envía al 2
+    private void actualizar_capacidadRestante_sensor(int sensorId1, int sensorId2) {
+        if (sensor.get(sensorId1).getCapacidad() > capacidadRestante[sensorId2]) {
+            info_perdida += (int) sensor.get(sensorId1).getCapacidad() - (int) capacidadRestante[sensorId2];
+            capacidadRestante[sensorId2] = 0;
+            sensor.get(sensorId2).setCapacidad((int) sensor.get(sensorId2).getCapacidad() + (int) capacidadRestante[sensorId2]);
+        }
+        else {
+            capacidadRestante[sensorId2] -= sensor.get(sensorId1).getCapacidad();
+            sensor.get(sensorId2).setCapacidad((int) sensor.get(sensorId2).getCapacidad() + (int) sensor.get(sensorId1).getCapacidad());
+        }
+    }
+
     private double dist(int id1, int id2, boolean escentro) {
         double x1, y1, x2, y2;
 
@@ -87,7 +175,7 @@ public class Estado implements Cloneable {
             x2 = sensor.get(id2).getCoordX();
             y2 = sensor.get(id2).getCoordY();
         }
-        return Math.sqrt((Math.pow(2,(x1-x2))) + (Math.pow(2,(x2-y2))));
+        return Math.sqrt((Math.pow(x1-x2, 2)) + (Math.pow(y1-y2,2)));
     }
 
     /*Métodos públicos*/
@@ -155,24 +243,25 @@ public class Estado implements Cloneable {
         return dfs(sensorId, visitado);
     }
 
+    public int getInfo_perdida() {
+        return this.info_perdida;
+    }
+
     /* esta función genera el estado inicial aleatoriamente */
     public void estado_inicial_random () {
         Random rand = new Random();
-        for(int i = 0; i < sensor.size(); i++){
-            capacidadRestante[i] = sensor.get(i).getCapacidad()*2;
-        }
+
         for (int i = 0; i < sensor.size(); i++) {
             int centroId = sensor.size() + rand.nextInt(centros.size());
             if (es_valido_centros(centroId)) {
                 conexiones[i] = centroId;
                 ++contador_conexiones[centroId];
+                actualizar_capacidadRestante_centro(i, centroId);
             } else {
                 int sensorId = rand.nextInt(sensor.size());
-                while (!es_valido_sensor(sensorId) && isValid(i, sensorId)) {
+                while (!es_valido_sensor(sensorId)) {
                     sensorId = rand.nextInt(sensor.size());
-                    capacidadRestante[sensorId] -= sensor.get(i).getCapacidad();
-                    sensor.get(sensorId).setCapacidad((int)sensor.get(i).getCapacidad() + (int) sensor.get(sensorId).getCapacidad());
-
+                    actualizar_capacidadRestante_sensor(i, sensorId);
                 }
                 conexiones[i] = sensorId;
                 ++contador_conexiones[sensorId];
@@ -201,44 +290,38 @@ public class Estado implements Cloneable {
                 ++contador_conexiones[id_min];
             }
 
-            else { // si no se puede conectar a un centro entonces se conecta a un sensor random
-                Random rand = new Random();
-                int sensorId = rand.nextInt(sensor.size());
-                while (!es_valido_sensor(sensorId) & isValid(i, sensorId)) {
-                    sensorId = rand.nextInt(sensor.size());
-                    capacidadRestante[sensorId] -= sensor.get(i).getCapacidad();
-                    sensor.get(sensorId).setCapacidad((int)sensor.get(i).getCapacidad() + (int) sensor.get(sensorId).getCapacidad());
+            else { // si no se puede conectar a un centro entonces se conecta al sensor o al centro más cercano
+                boolean[][] visitado = new boolean[100][100];
+                for (int l = 0; l < 100; l++) {
+                    Arrays.fill(visitado[l], false);
                 }
-                conexiones[i] = sensorId;
-                ++contador_conexiones[sensorId];
+                ArrayList<Integer> cercanos = bfs(i, visitado);
+                boolean encontrado = false;
+                int k = 0;
+                while (!encontrado && k < cercanos.size()) {
+                    if (es_centro(cercanos.get(k))) {
+                        if(es_valido_centros(cercanos.get(k))) {
+                            encontrado = true;
+                            conexiones[i] = cercanos.get(k);
+                            ++contador_conexiones[cercanos.get(k)];
+                            actualizar_capacidadRestante_centro(i, cercanos.get(k));
+                        }
+                    }
+                    else if (es_valido_sensor(cercanos.get(k))) {
+                        conexiones[i] = cercanos.get(k);
+                        ++contador_conexiones[cercanos.get(k)];
+                        actualizar_capacidadRestante_sensor(i, cercanos.get(k));
+                    }
+                }
             }
         }
-    }
-
-    public boolean isValid(int c1, int c2){
-        double info = sensor.get(c1).getCapacidad();
-        return info <= capacidadRestante[c2];
     }
 
     /*
      * A ACABAR
      */
-    public Estado clone() {
+    /*public Estado clone() {
 
-    }
-    /* mover estos métodos a la clase del algoritmo
-    public double cantidad_a_transmitir(int sensorId) {
-        double capacidad = sensor.get(sensorId).getCapacidad();
-        return capacidad*3;
-    }
-
-    public double cantidad_a_recibir(int sensorId) {
-        return sensor.get(sensorId).getCapacidad();
-    }
-
-    public double cantidad_a_almacenar(int sensorId) {
-        double capacidad = sensor.get(sensorId).getCapacidad();
-        return capacidad*2;
     }*/
 
 }
